a29bff8e21c4bd889c048f780fac920b85d4b980
wdw <wdw@82696.com>
2022-02-13 21:33:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# test too many git lfs file
+
+* a dir has 1k file
+* run `./init.sh 10` to generate many file
+* git lfs track "*.bin"
---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 1k file per dir
+
+if (( $# != 1 )); then
+	echo "Usage: $0 dircnt " 1>&2
+	exit 1
+fi
+
+dircnt=$1
+
+for (( i=1; i<=dircnt; ++i )); do
+	dir=$(printf %03d $i)
+	echo dir: $dir
+	mkdir -p $dir
+	cd $dir
+	for (( j=i; j<=1024; ++j )); do
+		file=$(printf "%s_%04d" $dir $j).bin
+		echo file: $file
+		if [[ ! -e $file ]]; then
+			dd if=/dev/urandom of=$file bs=1k count=1
+		fi
+	done
+	cd ..
+done
